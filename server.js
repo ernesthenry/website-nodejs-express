b0956e302a08dd482b32faa00adf534fe5ec0454
ernesthenry <henry38ernest@gmail.com>
2020-03-29 23:57:05 +0300

    server HTML pages and static content

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
+const path = require('path')
 
 const app = express()
 
 const port = 3000;
 
-app.get('/', (response, request) => {
-    response.send('Hello exress: )')
+app.use(express.static(path.join(__dirname, './static')))
+
+app.get('/', (request, response) => {
+    response.sendFile(path.join(__dirname, './static/index.html'))
+})
+
+app.get('/speakers', (request, response) => {
+    response.sendFile(path.join(__dirname, './static/speakers.html'))
 })
 
 app.listen(port, () =>{
