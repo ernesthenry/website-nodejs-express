6247a2df49c5bed8d9f4d3a3d17c4426ba3bcf8d
ernesthenry <henry38ernest@gmail.com>
2020-03-30 00:44:27 +0300

    set up eslint, prettier and nodemon

--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-const express = require('express')
-const path = require('path')
+const express = require('express');
+const path = require('path');
 
-const app = express()
+const app = express();
 
 const port = 3000;
 
-app.use(express.static(path.join(__dirname, './provided/static')))
+app.use(express.static(path.join(__dirname, './provided/static')));
 
 app.get('/', (request, response) => {
-    response.sendFile(path.join(__dirname, '.provided/static/index.html'))
-})
+  response.sendFile(path.join(__dirname, '.provided/static/index.html'));
+});
 
 app.get('/speakers', (request, response) => {
-    response.sendFile(path.join(__dirname, '.provided/static/speakers.html'))
-})
+  response.sendFile(path.join(__dirname, '.provided/static/speakers.html'));
+});
 
-app.listen(port, () =>{
-    console.log(`Express server listening on port ${port}!`)
-})
+app.listen(port, () => {
+  console.log(`Express server listening on port ${port}!`);
+});
